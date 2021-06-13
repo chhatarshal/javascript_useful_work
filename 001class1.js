075d9d6d03fccf6e121d367a60f3ddf16e35e595
Chhatarshal Singh <chauhan.natwal@hotmail.com>
2021-06-13 10:17:50 +0530

    closure important link to understand javascript

--- 001class1.js
+++ 001class1.js
@@ -86,3 +86,78 @@ function outside(x) {
   fn_inside = outside(3)(18);
   console.log(fn_inside);
 
+
+
+  console.log('understanding closue in java scripts')
+
+
+  console.log('create a simple function')
+
+  var pet = function (name) {
+
+    console.log(name)
+
+  }
+
+  var pet = function (name) {
+
+    function newpet() {
+      return name;
+    }
+
+    return newpet
+  }
+
+mupt =   pet('salu2')
+console.log(mupt())
+
+
+var test = function (name) {
+
+  var sex;
+
+  return {
+    getName : function() {
+      return name
+    },
+
+    getSex : function() {
+      return sex
+    },
+    setSex : function(sex1) {
+       sex = sex1
+    }
+  }
+}
+
+
+t = test('namewvalue')
+console.log(t.getName())
+console.log(t.getSex())
+console.log(t.setSex('male'))
+console.log(t.getSex())
+
+
+var getCode = function() {
+  var apiCode = '0]Eal(eh&2';    // A code we do not want outsiders to be able to modify...
+
+  return function() {
+    return apiCode;
+  };
+}()
+console.log(getCode())
+
+getCode();    
+
+console.log('Pit fall of closure')
+
+var createPet = function(name) {  // The outer function defines a variable called "name".
+  return {
+    setName: function(name) {    // The enclosed function also defines a variable called "name".
+      name = name;               // How do we access the "name" defined by the outer function?
+    }
+  }
+}
+
+v = createPet('a')
+console.log(v)
