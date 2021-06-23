a13923019e2fda5d8dbcf9d9bc6e0b12c910f068
Chhatarshal Singh <chauhan.natwal@hotmail.com>
2021-06-23 23:33:22 +0530

    java script simple object creation and accessing

---
+++ 001javascript_objects.js
@@ -0,0 +1,19 @@
+console.log('Creating simple javascript object');
+obj = {};
+console.log(obj)
+
+console.log('adding some properties to empty object');
+obj = {
+    name:'firstname',
+    id:'random0111'
+};
+console.log(obj);
+
+console.log('Exploring different way of accessing object properties');
+console.log(obj.name);
+console.log(obj.id);
+
+console.log(obj['name']);
+console.log(obj['id']);
+
+console.log('Object in javascript is similar to Map in javascript');
